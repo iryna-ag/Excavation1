4ae8cb77eaf83c0158ecddfd6c97cbc719048068
Iryna Agafonov <96760664+iryna-ag@users.noreply.github.com>
2022-09-11 16:58:48 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector('form').onsubmit = e => {
+    e.target.submit();
+    e.target.reset();
+    return false;
+ };
+ document.querySelector('form').onsubmit = e => {
+    e.target.submit();
+    e.target.reset();
+    return false;
+ };
