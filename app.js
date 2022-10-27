7a6e417bbf6c266a64507115e1a9dc690dd18d13
Iryna Agafonov <96760664+iryna-ag@users.noreply.github.com>
2022-10-26 23:27:39 -0400

    Spelling

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 function myFunction() {
     document.getElementById("myForm").reset();
 }
+
+    var btn = document.getElementsByClassName("btn");
+    var  banner = document.getElementById("ima");
+    
+    btn[0].onclick = function(){
+        banner.src = "images/project1-min.png";
+        animation();
+        this.classList.add("active");
+    }
+    
+    btn1[1].onclick = function(){
+        banner.src = "images/project2-min.jpeg";
+        animation();
+        this.classList.add("active");
+    }
+    
+    btn[2].onclick = function(){
+        banner.src = "images/project6-min.jpeg";
+        animation();
+        this.classList.add("active");
+    }
+    
+    
+    
+    function animation(){
+        banner.classList.add("zoom");
+        setTimeout(function(){
+            banner.classList.remove("zoom");
+        },500);
+        
+        for(b of btn){
+            b.classList.remove("active");
+        }
+    }
